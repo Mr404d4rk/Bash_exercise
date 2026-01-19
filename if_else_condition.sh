de85a790ac96f195bcc3edb0bdc13dd5e438621b
Mr404d4rk <yoursbhone@gmail.com>
2026-01-19 18:06:21 +0630

    changeee

--- if_else_condition.sh
+++ if_else_condition.sh
@@ -10,3 +10,33 @@ fi
 
 
 #If else shortcut
+[[ "Bhone Kyaw" == "Bhone Kyaw" ]] && echo "True" #True ko check ml so &&
+[[ "Bhone Kyaw " == "Kyaw Bhone" ]] || echo "False" #False condition ko check ml so ||
+
+#Dir Check
+
+if [[ -d /tmp ]];then
+echo "Exist"
+else
+echo "Doesn't Exist"
+fi
+
+
+#File Check 
+if [[ -e /etc/passwd ]];then
+echo "Exist"
+else
+echo "Doesn't Exist"
+fi
+#Permission Check
+if [[ -r /etc/passwd ]];then
+echo "Can read,wirte and execute"
+else
+echo "Can't read"
+fi
+
+if ! ls /tmp >/dev/null; then
+echo "file exist"
+else
+echo "file doesn't exist"
+fi
