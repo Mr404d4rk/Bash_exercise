f3ec4aab3311e03ac625581d62f419abc6ed3859
Mr404d4rk <yoursbhone@gmail.com>
2026-01-19 17:53:17 +0630

    change 1

---
+++ if_else_condition.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+x=10
+y=10
+if [[ $x == $y ]];then
+echo "The values of variable x and y are equal."
+else 
+echo "The values of variable x and y are not equal."
+fi 
+
+
+#If else shortcut
